893ae51ecababe1220743c8070a9c8d8d68d1393
scooby-kz <scoobybest5@gmail.com>
2023-12-08 20:11:23 +0600

    hhh

--- gitpush.sh
+++ gitpush.sh
@@ -5,9 +5,9 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
-
+#echo 'Enter the name of the branch:'
+#read branch
+branch=$(git branch | cut -c3- | dmenu -p "Select the branch to push: ")
 git push origin $branch
 
 read
