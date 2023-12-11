f3a9d317994e5d09996d4b8738d4246b5711063c
scooby-kz <scoobybest5@gmail.com>
2023-12-11 16:04:58 +0600

    edited

--- gitpush.sh
+++ gitpush.sh
@@ -5,9 +5,10 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
+#echo 'Enter the name of the branch:'
+#read branch
+
 #branch=$(git branch | cut -c3- | dmenu -p "Select the branch to push: ")
-git push origin $branch
+git push origin | git branch --show-current #$branch
 
 read
